7042f346e4b157cef643427366a5eadcfb37428d
Pru0w0 <6430178621@student.chula.ac.th>
2025-04-15 11:45:10 +0700

    addDataAndDeleteUnunsedLine

--- HPA_Project_ortools.py
+++ HPA_Project_ortools.py
@@ -19,28 +19,20 @@ def write_output(output_path, solution):
 
 def solve_power_plant_problem(num_cities, adj):
     solution = [1 for _ in range(num_cities)]
-    # Create the CP-SAT model
+
     model = cp_model.CpModel()
     
-    # Create variables: 1 if a power plant is placed in city i, 0 otherwise
     power_plants = [model.NewBoolVar(f'power_plant_{i}') for i in range(num_cities)]
     model.Minimize(sum(power_plants))
-    
-    # Add constraints: Each city must be covered either by its own power plant
-    # or by a power plant in an adjacent city
+
     for i in range(num_cities):
         line = adj[i]
         model.Add(sum(power_plants[i] + power_plants[n] for n in line) >= 1)
-    
-    # Objective: Minimize the total number of power plants
-    model.Minimize(sum(power_plants))
-    
-    # Solve the model
+
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
     
     if status == cp_model.OPTIMAL:
-        # Get the solution as a binary string
         solution = ''.join('1' if solver.Value(power_plants[i]) else '0' for i in range(num_cities))
         return solution
     else:
